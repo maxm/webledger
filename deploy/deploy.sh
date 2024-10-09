ea1f75e3fd6f75bbe04f43800d30243dc4ff54a2
Max <max@pomelogames.com>
2024-10-09 17:00:53 -0300

    faster deploy

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -2,11 +2,11 @@ GOOS=linux GOARCH=386 CGO_ENABLED=0 go build -o build/main.linux main.go tokens.
 ssh server <<'ENDSSH'
   mkdir -p /var/www/webledger/
 ENDSSH
-scp build/main.linux server:/var/www/webledger/main.linux.next
-scp -r public/ server:/var/www/webledger/
-scp -r templates/ server:/var/www/webledger/
-scp ledgers.json server:/var/www/webledger/
-scp deploy/webledger.service server:/etc/systemd/system/
+rsync -avP build/main.linux server:/var/www/webledger/main.linux.next
+rsync -avP -r public server:/var/www/webledger/
+rsync -avP templates server:/var/www/webledger/
+rsync -avP ledgers.json server:/var/www/webledger/
+rsync -avP deploy/webledger.service server:/etc/systemd/system/
 ssh server <<'ENDSSH'
   systemctl stop webledger
   mv /var/www/webledger/main.linux.next /var/www/webledger/main.linux
